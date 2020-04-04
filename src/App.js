bdb54ad52deaa2008e2e934642c110e78f406f04
jimin <seojiminca@gmail.com>
2020-04-04 17:12:22 +0900

    add prop-types

--- src/App.js
+++ src/App.js
@@ -3,27 +3,37 @@ import Potato from "./potato";
 
 const foodILike = [
     {
+        id: 1,
         name: "Kimchi",
+        rate: 3,
         image:
             "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
     },
     {
+        id: 2,
         name: "Samgyeopsal",
+        rate: 5,
         image:
             "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
     },
     {
+        id: 3,
         name: "Bibimbap",
+        rate: 2.6,
         image:
             "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
     },
     {
+        id: 4,
         name: "Doncasu",
+        rate: 5,
         image:
             "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
     },
     {
+        id: 5,
         name: "Kimbap",
+        rate: 3.5,
         image:
             "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
     }
@@ -34,7 +44,12 @@ function App() {
     <div>
       <h1>jimin app</h1>
         {foodILike.map(dish => (
-            <Potato name={dish.name} image={dish.image}/>
+            <Potato
+                key={dish.id}
+                name={dish.name}
+                image={dish.image}
+                rate={dish.rate}
+            />
         ))}
     </div>
   );
--- src/potato.js
+++ src/potato.js
@@ -1,14 +1,22 @@
 import React from "react";
+import PropTypes from "prop-types";
 
                 //매개변수에 {}사용 -> 사용자입력값.
-function Potato({name,image}) {
+function Potato({name,image,rate}) {
                 //html 안에 {} -> JavaScript
     return (
         <div>
             <h2>I like {name}</h2>
+            <h3>{rate} / 5.0</h3>
             <img src={image} />
         </div>
     );
+}
+
+Potato.propTypes = {
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    rate: PropTypes.number
 }
 
 export default Potato;
